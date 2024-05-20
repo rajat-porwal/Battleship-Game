9a8c083ce9def8a6f27ab437921259071a365862
Rajat Porwal <rajatporwal@protonmail.com>
2024-05-20 10:44:07 +0530

    add a method isSunk() in ship module that determines if a ship is afloat based on its length

--- src/shipBuilding.js
+++ src/shipBuilding.js
@@ -5,9 +5,13 @@ class shipBuilding {
         this.sunkStatus = sunkStatus;
     }
 
-    shipHit() {
+    shipHit() { //confused whether to add the condition for hit++ only if its less than ship size or leave it to be handled by other modules
         this.timesHit++;
     }
     
-    
+    isSunk() {
+        if(this.timesHit == this.length){
+            this.sunkStatus = true;
+        }
+    }
 }
