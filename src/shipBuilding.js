afc49ba5773c682009bb24f66b6e8f5a773e4924
Rajat Porwal <rajatporwal@protonmail.com>
2024-05-20 10:32:08 +0530

    add another module that contain the ship class

---
+++ src/shipBuilding.js
@@ -0,0 +1,7 @@
+class shipBuilding {
+    constructor(length, timesHit, sunkStatus){
+        this.length = length;
+        this.timesHit = timesHit;
+        this.sunkStatus = sunkStatus;
+    }
+}
